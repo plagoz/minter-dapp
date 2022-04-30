d8eedc42391631898a375b4e337a6e942ea23082
plagoz <plagoz@yahoo.com>
2022-04-30 15:36:33 +0700

    edit

--- backend/src/config.js
+++ backend/src/config.js
@@ -78,7 +78,7 @@ const TOKENS_PER_MINT = 1; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-05-05T12:00:00+07:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-04-02T21:00:00+07:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x3e458775421Ef1A9B23729E33A76ba0A12F823Ff"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
